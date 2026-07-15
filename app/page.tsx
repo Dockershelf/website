0dcb86713d69cc3096bc65b1c4ff29b03b76723c
Luis Alejandro Martínez Faneyth <luis@luisalejandro.org>
2026-07-15 15:00:30 -0400

    style: minor formatting cleanup in app/page.tsx and Container.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,6 @@ import StyledLink from "@components/common/StyledLink";
 import ButtonBar from "@components/Home/ButtonBar";
 import { HomeContentSections } from "@components/Home/HomeContentSections";
 
-
 export const metadata: Metadata = {
   title: siteConfig.name,
   description: siteConfig.description,
